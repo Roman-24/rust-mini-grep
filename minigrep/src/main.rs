e9e291722da84a9af04cb1234c1732878dc7fad8
roman <xromanb@proton.me>
2024-08-19 14:35:23 +0200

    REF: create object for input params

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,23 +1,30 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn main() {
     println!("Rust example of grep!");
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_args(&args);
+    let config = parse_args(&args);
 
-    println!("Searching for: {query}");
-    println!("In file: {file_path}");
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
     print!("Text:");
     print!("{contents}");
 }
 
-fn parse_args(args: &[String]) -> (String, String) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query.to_string(), file_path.to_string())
+fn parse_args(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    return Config { query, file_path };
 }
