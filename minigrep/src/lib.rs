92a27b1e64c60168ff9a37cf5dd270fcf16d9de1
roman <xromanb@proton.me>
2024-08-19 18:15:53 +0200

    FEAT: add search function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -19,11 +19,28 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
     print!("Text:");
     print!("{contents}");
+
+    let search_result = search(&config.query, &contents);
+    for line in search_result {
+        println!("{}", line);
+    }
 
     Ok(())
 }
