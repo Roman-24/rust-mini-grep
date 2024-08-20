a7709e950550990a1e0e3ae8aa0f234a65e93cb1
roman <xromanb@proton.me>
2024-08-20 10:40:46 +0200

    FEAT: let choose if search case sensitive

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub is_case_sensitive: bool,
 }
 
 impl Config {
@@ -15,11 +17,18 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        return Ok(Config { query, file_path });
+        let is_case_sensitive = env::var("IS_CASE_SENSITIVE").is_ok();
+
+        return Ok(Config {
+            query,
+            file_path,
+            is_case_sensitive,
+        });
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    println!("Searching w case sensitive.");
     let mut results = Vec::new();
 
     for line in contents.lines() {
@@ -31,10 +40,29 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    println!("Searching w case insensitive.");
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    let search_result = search(&config.query, &contents);
+    let search_result = if config.is_case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
     println!("Results:");
     for line in search_result {
         println!("{}", line);
